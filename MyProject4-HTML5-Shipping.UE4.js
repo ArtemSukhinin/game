2346f852c482034658011119a8bfeed8828a220d
ArtemSukhinin <103842361+ArtemSukhinin@users.noreply.github.com>
2022-04-17 23:16:07 +1000

    Update MyProject4-HTML5-Shipping.UE4.js

--- MyProject4-HTML5-Shipping.UE4.js
+++ MyProject4-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/ArtemSukhinin/game/0664a62c5845b40c8752a877e8716f224a73191f/MyProject4-HTML5-Shipping.data.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
@@ -1155,7 +1155,7 @@ $(document).ready(function() {
 		// ----------------------------------------
 		// MORE JS
 		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://dl.dropboxusercontent.com/s/pdz0951abcm5qm4/MyProject4-HTML5-Shipping.data?dl=0'));
-		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
+		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/ArtemSukhinin/game/0664a62c5845b40c8752a877e8716f224a73191f/Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
 			fetchOrDownloadAndStore(db, Module.locateFile('MyProject4-HTML5-Shipping.data')).then(function(dataBlob) {
